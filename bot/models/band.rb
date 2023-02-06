b9c8b8e3f3779525dc8ab4c9c36669aa51e9f674
ilyaklapatok <klapatok1980@gmail.com>
2023-02-06 17:05:14 +0300

    Correct band's score method

--- bot/models/band.rb
+++ bot/models/band.rb
@@ -7,6 +7,8 @@ class Band < BotRecord
   validates :foundation_year, numericality: { only_integer: true, greater_than: 1800, less_than: Date.today.year }
 
   def score
+    return 0 if ratings.blank?
+
     scores = ratings.pluck(:score)
     scores.sum(0) / scores.size
   end
